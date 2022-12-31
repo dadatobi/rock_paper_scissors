2661a44eca27c06bc90be572b63b7839bc888fd1
Dada Oluwatobi <dadatobi66@gmail.com>
2022-12-31 19:05:43 +0100

    finished the javascript file

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+computerScore = 0; 
+playerScore= 0;
+
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
-        return "rock"
+        return "Rock"
     }else if (randomNumber === 2) {
-        return "paper"
+        return "Paper"
     } else {
-        return "scissors"
+        return "Scissors"
     }
 }
 
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
 function playerSelection() {
     // Use prompt to get user input
     const input = prompt("please enter your choice between 'Rock, Paper or Scissors': ")
@@ -26,20 +29,37 @@ function playerSelection() {
         return playerSelection();
     }
 }
-console.log(playerSelection())
+// console.log(playerSelection())
 
 function playRound(player,computer) {
     // compare player and computer selection 
-    // if (player === "Rock" && computer === "Scissor"){
-
-    // }
-    // Rock beat scissors
-    // scissors beat paper
-    // paper beat rock
+    let n = 0
+    if ((player === "Rock" && computer === "Scissors") ||(player === "Scissors" && computer === "Paper") || (player === "Paper" && computer === "Rock")){
+        playerScore++;
+        // console.log(playerScore);
+        return `Winner!! ${player} beats ${computer}`;
+     }else if((player === "Scissors" && computer === "Rock") ||(player === "Paper" && computer === "Scissors") || (player === "Rock" && computer === "Paper")) {
+        computerScore++;
+        return `You lose! ${computer} beats ${player}`;
+     } else if (player === computer) {
+        return `Tie game`;
+     }
 }
+
+// console.log(playRound(playerSelection(), getComputerChoice()));
 
 function game() {
     // plays five round of games
-    // keep scores
-    // declare the winner
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection(), getComputerChoice()));
+    }
+    if(playerScore > computerScore){
+        console.log("you win😊!!!")
+    }else if(computerScore > playerScore){
+        console.log("Computer won!!!")
+    }else {
+        console.log ("It's a tie game")
+    }
 }
+
+game();
